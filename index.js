677a0b9cf4b2ad9d164c55fa0d51a414f8e790f2
Neel Patel <neelp2023@gmail.com>
2021-04-26 17:37:36 -0500

    CORS Policy

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const app = express();
 const sampleData = require("./data.json");
 let port = process.env.PORT || 8000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.get("/", (req, res) => {
-    res.header('Access-Control-Allow-Origin')
-    res.send("Hello World")
+  res.send("Hello World")
 })
 
 app.get("/users", (req, res) => {
-    res.send(sampleData)
+  res.send(sampleData)
 })
 
 app.listen(port, () => {
-    console.log(`Serenify Backend Server is running on ${port}`)
+  console.log(`Serenify Backend Server is running on ${port}`)
 })
