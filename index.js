da7ddb133cdbb0d5857e7c953120aca8e7b76e8c
Neel Patel <neelp2023@gmail.com>
2021-04-26 17:24:23 -0500

    Route Changes

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
-const sampleData = require("./data.json");
-let port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/users", (req, res) => {
-    res.send(sampleData)
-})
+// API calls
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
 
-app.listen(port, () => {
-    console.log(`Serenify Backend Server is running on ${port}`)
-})
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
